1360c3a0ae50cfd2f30136f851b1bcfbb673f56a
jeff <jeffyjkang@gmail.com>
2021-12-17 22:11:38 -0600

    init

---
+++ src/routes/[searchTerm]/search-result.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+  import {page} from '$app/stores';
+  import {onMount} from 'svelte';
+  let songResults = [];
+
+  onMount(async () => {
+    let searched = $page.params.searchTerm;
+    const itunesSearched = await fetch(`https://itunes.apple.com/search?term=${searched}&entity=song`);
+    let res = await itunesSearched.json();
+    songResults = res.results;
+    console.log(songResults);
+  })
+</script>
+
+<div>
+  {$page.params.searchTerm}
+</div>
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+  import {goto} from '$app/navigation';
+  let searchTerm = '';
+  const submitSearch = () => {
+    console.log('submitHit');
+    goto(`${searchTerm}/search-result`)
+  }
+</script>
+
+<div>
+  <div>Search an Artist</div>
+    <form on:submit|preventDefault={submitSearch}>
+      <input type='text' bind:value={searchTerm} />
+      <button type='submit'>Search</button>
+    <form />
+  <p>{searchTerm}</p>
+</div>
